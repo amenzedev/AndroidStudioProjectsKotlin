1356da6705b8db227da7534cfcac2a233ae9b3ce
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-08-30 21:24:45 -0400

    challenge remove duplicates

---
+++ Ex_Files_Kotlin_Essential_Training/Exercise Files/src/main/kotlin/chapter4/_04_07/begin/challenge_remove_duplicates.kt
@@ -0,0 +1,9 @@
+package chapter4._04_07.begin
+
+fun main() {
+    val animals =
+        listOf("apple", "biscuit", "apple", "cat", "cat", "cat",
+            "dog", "elephant", "fox", "goat", "elephant")
+    val animalSet = animals.toSet()
+    println(animalSet)
+}
