cf9165da118b0de8dcb49329b938db5ff3f036bc
AmanuelGhebreyohannes <amageb2014@gmail.com>
2022-09-06 21:37:46 -0400

    challenge count instances

---
+++ Ex_Files_Kotlin_Essential_Training/Exercise Files/src/main/kotlin/chapter7/_07_06/begin/challenge_count_instances.kt
@@ -0,0 +1,28 @@
+package chapter7._07_06.begin
+
+class Bosco {
+    companion object{
+        private  var count = 0
+        fun increment(){
+            count++
+        }
+        fun show(){
+            println("$count instances created")
+        }
+    }
+    init {
+        increment()
+    }
+}
+
+fun nop() {
+    val b = Bosco()
+}
+
+fun main() {
+    val b1 = Bosco()
+    val b2 = Bosco()
+    nop()
+    val b3 = Bosco()
+    Bosco.show()
+}
